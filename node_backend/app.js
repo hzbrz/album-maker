0dd128fd4558ff4f8cba0f9c7f5b12329a522aa1
Hasan Zahid <hasanthedev@gmail.com>
2019-07-21 23:20:29 -0500

    express start app

--- node_backend/app.js
+++ node_backend/app.js
@@ -1,1 +1,18 @@
 // this is the empty shell for the express app
+
+const express = require("express");
+const morgan = require("morgan")
+
+const app = express()
+
+app.use(morgan("dev"))
+
+app.get("/", (req, res) => {
+  res.send("learning how to hello REST API with js")
+})
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log("app is listening on port " + port)
+});
