94f059e71cdd1a5ca1b0cdfbbe0840a238adec0a
Hasan Zahid <hasanthedev@gmail.com>
2019-08-03 16:01:48 -0500

    trying to figure out the API part of login:

--- node_backend/controllers/authController.js
+++ node_backend/controllers/authController.js
@@ -4,6 +4,13 @@ exports.getUser = (req, res) => {
 
 // this function is going to take the data from social login (google login) then use that to sign the user into the api/server
 // using jwt in order to make the user have authentication with the program itself
-exports.login = (req, res) => {
-  
+exports.login = (req, res, next) => {
+  const email = req.body.email
+  const firstName = req.body.firstName
+  const lastName = req.body.lastName
+  const profile_pic = req.body.profile_image
+  const username = req.body.profile_name
+
+  let loadedUser;
+
 }
