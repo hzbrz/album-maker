3120deb8e10c7a02847f04222f498e3ad0782ecd
Hasan Zahid <hasanthedev@gmail.com>
2019-08-26 07:24:26 -0500

    delete backend

--- node_backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const isAuth = require("../middleware/is-auth");
-const userController = require("../controllers/userController");
-
-router.post("/room", isAuth, userController.createRoom);
-
-module.exports = router;
