8b666dfc3b96bb5481f6995769b0f0f8830630ad
Hasan Zahid <hasanthedev@gmail.com>
2019-08-26 00:27:49 -0500

    deleting users

--- node_backend/controllers/photoController.js
+++ node_backend/controllers/photoController.js
@@ -48,25 +48,29 @@ exports.getUserPhotos = (req, res, next) => {
     firestore.collection("albums").doc(albumId).get()
       .then(snap => {
         return new Promise((resolve, reject) => {
-          snap.data().photos.forEach(photo => {
+          for (const photo of snap.data().photos) {
+            // getting the photos from the photo collection using the ids from the album coll
             firestore.collection("photos").doc(photo.photoId).get()
-            .then(snap => {
-              userColl.doc(snap.data().creator).get()
-              .then(creator => {
-                let imageObj = {
-                  _id: photo.photoId,
-                  image: snap.data().image,
-                  creator: creator.data().name
-                }
-                imageArray.push(imageObj)
-                resolve(imageArray)
+              .then(snap => {
+                // need to get the user's name who took the photo
+                userColl.doc(snap.data().creator).get()
+                  .then(creator => {
+                    let imageObj = {
+                      _id: photo.photoId,
+                      image: snap.data().image,
+                      creator: creator.data().name,
+                      userId: creator.id
+                    }
+                    imageArray.push(imageObj)
+                    resolve(imageArray)
+                  })
+                  .catch(err => console.log("Cannot get the user object from db ", err))
               })
-              .catch(err => console.log("Cannot get the user object from db ", err))
-            })
-            .catch(err => console.log("cannot get the photo collection ", err))
-          })
+              .catch(err => reject("cannot get the photo collection ", err))
+          }
         })
         .then(imageArr => {
+          // resolved array of the object that I get from promise above
           res.status(200).json({ message: "Image array fetched", images: imageArr })
         })
         .catch(err => console.log("Cannot resolve the promise and get the array of photos ", err))
@@ -125,23 +129,20 @@ exports.storePhoto = (req, res, next) => {
 
 
 exports.deletePhoto = (req, res, next) => {
+  const userId = req.userId;
+  console.log(userId)
   // reading the id from the parameter of the request
   const photoId = req.params.photoId;
-  let userId = req.userId;
-  let albumId = req.albumId;
-  let image = req.originalUrl.split("?image=")[1];
-  let storage = image.split("/?filepath=");
-  const imageUrl = storage[0].trim();
-  const filepath = storage[1].replace("%20", " ");
+  const albumId = req.params.album;
+  const matchUser = req.params.matchUser;
   let firestore = firebase.firestore();
   let userDocRef = firestore.collection("users").doc(userId)
-  let imageObj = {
-    _id: photoId, filepath: filepath, image: imageUrl
-  }
 
-  if (!albumId) {
-    console.log("albumID is not found")
+  if (userId === matchUser) {
     userDocRef.update({ photos: firebase.firestore.FieldValue.arrayRemove(photoId) })
+    firestore.collection("albums").doc(albumId).update({
+      photos: firebase.firestore.FieldValue.arrayRemove({ photoId: photoId })
+    })
     firestore.collection('photos').doc(photoId).get()
       .then(snap => {
         res.json({ message: "User deleted", path: snap.data().filepath })
@@ -149,16 +150,6 @@ exports.deletePhoto = (req, res, next) => {
       })
       .catch(err => console.log("ERROR while getting photo from db ", err))
   } else {
-    firestore.collection("albums").doc(albumId).update({
-      photos: firebase.firestore.FieldValue.arrayRemove(imageObj)
-    })
-    res.json({ message: "User deleted", path: null })
-    // userDocRef.update({ photos: firebase.firestore.FieldValue.arrayRemove(photoId) })
-    // firestore.collection('photos').doc(photoId).get()
-    //   .then(snap => {
-    //     res.json({ message: "User deleted", path: null })
-    //     firestore.collection('photos').doc(photoId).delete();
-    //   })
-    //   .catch(err => console.log("ERROR while getting photo from db ", err))
+    res.json({ message: "Cannot delete, photo was not created by the user", path: null })
   }
 }
