0dcfd23b0276413dc76e70fff588166b12585d6f
Hasan Zahid <hasanthedev@gmail.com>
2019-08-28 02:55:39 -0500

    changing to cors npm module

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const firebase = require("firebase");
+const cors = require("cors");
 
 // express app
 const app = express();
@@ -25,23 +26,25 @@ app.use(bodyParser.json({ limit: "5mb" }))
 
 app.use(morgan("dev"))
 
-// adding a general middleware to add headers that prevent from causing a CORS error which is irrelevant while creating APIs
-app.use((req, res, next) => {
-  // EVERY RES SENT WILL NOW HAVE THESE HEADERS BELOW 
-  
-  // header to let know which domain can access our server, for this we are letting all servers with * wildcard
-  res.setHeader('Access-Control-Allow-Origin', "*")
-  
-  // letting the client know which http methods are allowed
-  res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE")
+app.use(cors())
 
-  // which headers the client can set, this lets the client send in the content-type and authorization headers which 
-  // are useful for requesting data to the server and API, also some default headers get passed in
-  res.setHeader('Access-Control-Allow-Headers', "Content-Type, Authorization")
+// // adding a general middleware to add headers that prevent from causing a CORS error which is irrelevant while creating APIs
+// app.use((req, res, next) => {
+//   // EVERY RES SENT WILL NOW HAVE THESE HEADERS BELOW 
   
-  // moving on to the next middleware
-  next();
-})
+//   // header to let know which domain can access our server, for this we are letting all servers with * wildcard
+//   res.setHeader('Access-Control-Allow-Origin', "*")
+  
+//   // letting the client know which http methods are allowed
+//   res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, PATCH, DELETE")
+
+//   // which headers the client can set, this lets the client send in the content-type and authorization headers which 
+//   // are useful for requesting data to the server and API, also some default headers get passed in
+//   res.setHeader('Access-Control-Allow-Headers', "Content-Type, Authorization")
+  
+//   // moving on to the next middleware
+//   next();
+// })
 
 
 app.use("/auth", authRoutes);
