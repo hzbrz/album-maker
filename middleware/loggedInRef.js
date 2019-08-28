dc68b9a603b324c13fd46658f883c626efd0c59a
Hasan Zahid <hasanthedev@gmail.com>
2019-08-28 02:25:30 -0500

    inv after login

--- middleware/loggedInRef.js
+++ middleware/loggedInRef.js
@@ -6,7 +6,27 @@ module.exports = (req, res, next) => {
   if (!albumId) {
     console.log("User was not redirected after invite")
   } else {
-
+    // get the albums ids from the album_ids coll that tarcks all album/rooms
+    firestore.collection("albums_ids").doc(albumIdArrSecret).get()
+      .then(snap => {
+        if (snap.data().ids.includes(album_id)) {
+          console.log("Album id found in the array, this room exists!")
+          // then setting
+          firestore.collection("users").doc(id).update({
+            albumUserPartOf: firebase.firestore.FieldValue.arrayUnion(album_id)
+          })
+          // sending resposne with the albumId for client side operations
+          res.status(200).json({ message: "New album added to the user's albums", albumId: album_id })
+        } else {
+          // if the albumId does not match any of the rooms that in the db then
+          console.log("Album id was not matched with anything in db albums")
+          res.status(200).json({ message: "No matching user albums were found", albumId: null })
+        }
+      })
+      .catch(err => console.log("Could not get the album/room ids ", err))
   }
+
+  req.currentAlbum = albumId
+
   next();
 }
